ce5d62182700d6037c35a645f0a423ead239e044
David Nassif <danassif@live.unc.edu>
2022-12-05 00:01:51 -0500

    Created args, port, dir, frontEndPages, styling

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ import path from 'path';
 import {fileURLToPath} from 'url';
 
 
-
+// Create database
 const db = new database('task.db');
 db.pragma('journal_mode = WAL');
 
-const app = express();
+// Connect app to front end pages in frontEndPages directory
+app.use(express.static("styling"));
 app.set('view engine', 'ejs');
 app.set('frontEndPages', path.join(__dirname, 'frontEndPages'));
+
+// Create all the needed constants
+const app = express();
+const args = minimist(process.argv.slice(2));
+const port = args.port || 2000
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
