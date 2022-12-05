f2612bb2e6005e37760745757b2796373af17d83
David Nassif <danassif@live.unc.edu>
2022-12-05 01:16:06 -0500

    API linking up and working

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 import express from 'express';
 import minimist from 'minimist';
-import Database from "better-sqlite3";
+import database from "better-sqlite3";
 import path from 'path';
 import {fileURLToPath} from 'url';
 
 
 // Create database
-//const db = new database('task.db');
-//db.pragma('journal_mode = WAL');
+const db = new database('task.db');
+db.pragma('journal_mode = WAL');
 
 
 // Create all the needed constants
 const args = minimist(process.argv.slice(2));
-const port = args.port || 2000
-
+const port = args.port || 4321
 
 // Connect app to front end pages in frontEndPages directory
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(express.static("styling"));
 app.set('view engine', 'ejs');
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.set('frontEndPages', path.join(__dirname, 'frontEndPages'));
 
 app.use(express.json());
@@ -43,12 +43,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.redirect('/homePage');
-  })
+});
+
+app.get('/homePage', function(req, res) {
+    res.render('homePage');
+});
   
 // app.get('/app', function (req, res) {
 //     res.redirect('/homePage');
 // })
-
 
 app.listen(port)
 
